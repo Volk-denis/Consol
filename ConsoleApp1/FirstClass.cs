ba79c5dd17f6b4d08367d3f8516200afc6589d40
batia-denis <batia-denis@outlook.com>
2018-08-12 23:30:14 +0300

    Dell

---
+++ ConsoleApp1/FirstClass.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    
+
+    class FirstClass {
+
+        private string name;
+        private string name2;
+
+        public FirstClass()
+        {
+
+        }
+
+        public FirstClass(string name, string name2)
+        {
+            this.name = name;
+            this.name2 = name2;
+
+        }
+
+        public string Name
+        {
+            get {
+                return name;
+            }
+            set {
+                this.name = value;
+                
+            }
+        }
+        public string Name2
+        {
+            get
+            {
+                return this.name2;
+            }
+            set
+            {
+                this.name2 = value;
+
+            }
+        }
+
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FirstClass f = new FirstClass("Den", "Batia");
+            Console.WriteLine(f.Name);
+            f.Name = "Vitia";
+            Console.WriteLine(f.Name);
+                
+            
+            Console.ReadKey();
+
+        }
+    
+       
+    }
+}
+
