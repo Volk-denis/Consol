a67a3c54edaf08239dd865d86012738ebe0b145e
batia-denis <batia-denis@outlook.com>
2018-08-12 23:42:05 +0300

    3

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -15,26 +15,7 @@ namespace ConsoleApp1{
 
         public static void Main(string[] arg) {
             
-            for(int q = 10; q > 0; q--)
-            {
-                Thread t = new Thread(new ThreadStart(Add));
-                t.Start();
-                              
-            }
-            Console.ReadKey();
-
-
-        }
-        public static void Add()
-        {
-            lock (locer)
-            while(a < 200)
-            {
-                
-                a++;
-                Console.WriteLine(a);
-                Thread.Sleep(10);
-            }
+            
         }
 
         
