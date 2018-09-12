99c6d04584325b3450e68949339c2fed44aad5ed
batia-denis <batia-denis@outlook.com>
2018-09-12 22:11:13 +0300

    Серрилизация

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,31 +9,35 @@ using System.Threading.Tasks;
 using System.Net;
 using System.IO;
 using System.Numerics;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
 
 namespace ConsoleApp1{
-
-    enum Figures { Romb, Rect, Triangle, Circle }
-    struct Fdata
-    {
-        public int x0, y0;
-        public int color;
-        public Figures type;
-    }
-
-
-    class Program
-    {
-
-       
-        
-        public static void Main()
-        {
-            Fdata fdata = new Fdata() { x0 = 4, y0 = 3, color = 1234, type = Figures.Romb };
-            Console.WriteLine(Area(fdata)?? 0);
             //поток доходит до асинк метода. Далее создается фоновой поток, который начинает работать
             //Основной далее выполняет действия до ключевого слова авэйт и выходит из метода ("ждет выполнеие таски"(не совсем так)). 
             //Как только фоновой поток закончит свою работу, основному потоку вернется управление в методе в котором создалась таска и
             //он завершит этот метод (все что после авэйт). Это касается и исключений.
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            MemoryStream memoryStream = new MemoryStream();
+
+            formatter.Serialize(memoryStream, "Ден хороший парень");
+
+            byte[] b = new byte[memoryStream.Length];
+            memoryStream.Position = 0;
+            while(memoryStream.Position < memoryStream.Length)
+            {
+                
+                b[memoryStream.Position++] = (byte)memoryStream.ReadByte();
+            }
+
+            memoryStream.Dispose();
+
+            
 
 
 
@@ -68,11 +72,12 @@ namespace ConsoleApp1{
             //Console.WriteLine(_struct.A);
             //checked
             //{
-            
-           
-                //Console.WriteLine( Factorial(6));
+
+
+            //Console.WriteLine( Factorial(6));
             //}
-            
+
+
 
             Console.ReadKey();
 
@@ -103,23 +108,7 @@ namespace ConsoleApp1{
         {
             return s < 1 ? 1 : s * Factorial(--s);
         }
-        public static double? Area(Fdata fd)
-        {
-            double res;
-            switch (fd.type)
-            {
-                case Figures.Romb :
-                    res = fd.x0 * fd.y0;
-                    return res;
-                case Figures.Circle:
-                    res = fd.x0 - fd.y0;
-                    return res;
-                default:
-                    return null;
-
-
-            }
-        }
+        
         
        
     }
